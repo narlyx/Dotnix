aacd78c2d55d2592431ca16a8bbe9b03f6f50942
Narlyx <109835029+narlyx@users.noreply.github.com>
2025-01-22 07:36:32 -0700

    Neovim findfile

--- home/narlyx/dotfiles/neovim/lua/narlyx/plugins/telescope.lua
+++ home/narlyx/dotfiles/neovim/lua/narlyx/plugins/telescope.lua
@@ -28,7 +28,7 @@ return {
 				},
 			})
 			local builtin = require("telescope.builtin")
-			-- vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<C-o>", builtin.find_files, {})
 			vim.keymap.set(
 				"n",
 				"<C-p>",
