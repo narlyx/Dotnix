727e9471c229112cff80de0d7c9bd2723731846d
Narlyx <109835029+narlyx@users.noreply.github.com>
2025-02-15 11:54:16 -0700

    Nixos garbage collection

--- hosts/nixos/common/system/nixSettings.nix
+++ hosts/nixos/common/system/nixSettings.nix
@@ -1,4 +1,18 @@
 {
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.autoUpgrade = {
+    enable = true;
+    dates = "weekly";
+  };
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 3d";
+    };
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
 }
