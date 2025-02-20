200512dda138101c6ad322167f21db628b116097
Narlyx <109835029+narlyx@users.noreply.github.com>
2025-02-20 09:37:04 -0700

    Git fixes + pager

--- home/narlyx/nixos/common/features/git/default.nix
+++ home/narlyx/nixos/common/features/git/default.nix
@@ -1,16 +1,24 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     gh
-    bat
   ];
 
   programs.git = {
     enable = true;
+
     userName = "Narlyx";
     userEmail = "109835029+narlyx@users.noreply.github.com";
+
+    delta = {
+      enable = true;
+      options = {
+        line-numbers = true;
+        side-by-side = true;
+      };
+    };
+
     extraConfig = {
-      credential.helper = "gh auth git-credential";
-      core.pager = "bat";
+      credential.helper = "${pkgs.gh}/bin/gh auth git-credential";
     };
   };
 }
