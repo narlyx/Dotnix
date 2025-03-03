77c5c73970e86a2f6b3c8fbcddda7f0754a626f6
Narlyx <109835029+narlyx@users.noreply.github.com>
2025-03-03 08:47:13 -0700

    Separated plasma into its own option

--- hosts/nixos/arsenic/default.nix
+++ hosts/nixos/arsenic/default.nix
@@ -4,6 +4,7 @@
     /etc/nixos/apple-silicon-support
     ../common/system
     ../common/users
+    ../common/environments/plasma.nix
     ../common/features/tailscale.nix
   ];
   home-manager.users."narlyx".imports = [../../../home/narlyx/nixos/asahi.nix];
@@ -11,11 +12,6 @@
   networking.hostName = "arsenic-linux";
 
   boot.kernelParams = ["apple_dcp.show_notch=1"];
-
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.desktopManager.plasma6.enable = true;
 
   system.stateVersion = "25.05"; # Did you read the comment?
 }
