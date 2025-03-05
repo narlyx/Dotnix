83f98798d3501dc3e0c09dfaefa59c3004e7fd11
Narlyx <109835029+narlyx@users.noreply.github.com>
2025-03-05 11:48:15 -0700

    Changed Wezterm theme

--- home/narlyx/common/features/wezterm/default.nix
+++ home/narlyx/common/features/wezterm/default.nix
@@ -6,7 +6,7 @@
       local wezterm = require 'wezterm'
       local config = wezterm.config_builder()
 
-      config.color_scheme = 'catppuccin-frappe'
+      config.color_scheme = 'catppuccin-mocha'
       config.window_background_opacity = 0.9
       config.hide_tab_bar_if_only_one_tab = true
 
