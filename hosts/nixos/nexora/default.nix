95e9fa6443f498cf4e09b624d2974f012244855b
Narlyx <109835029+narlyx@users.noreply.github.com>
2025-03-06 00:49:24 -0700

    Nexora fixes

--- hosts/nixos/nexora/default.nix
+++ hosts/nixos/nexora/default.nix
@@ -5,22 +5,19 @@
     ../common/system
     ../common/users
     ../common/environments/hyprland.nix
-    ../common/features/virtualisation.nix
     ../common/features/tailscale.nix
     ../common/features/printing.nix
   ];
   home-manager.users."narlyx".imports = [../../../home/narlyx/nixos];
 
-  # Hostname
+  # Host name
   networking.hostName = "nexora";
 
   # Touchpad fix
   environment.etc."modprobe.d/psmouse.conf".text = ''
     options psmouse synaptics_intertouch=1
   '';
-
-  # Other services
-  services.fwupd.enable = true;
+  # If mouse stops working: sudo rmmod psmouse && modprobe psmouse
 
   # System version
   system.stateVersion = "24.11";
