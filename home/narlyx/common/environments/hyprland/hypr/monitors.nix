9cde77f013936ca61d95c402840b565fe5f0767b
Narlyx <109835029+narlyx@users.noreply.github.com>
2025-03-10 10:10:28 -0600

    Acetylene monitors

--- home/narlyx/common/environments/hyprland/hypr/monitors.nix
+++ home/narlyx/common/environments/hyprland/hypr/monitors.nix
@@ -4,9 +4,10 @@
       # Acetylene
       if osConfig.networking.hostName == "acetylene"
       then [
-        "HDMI-A-1, 1920x1080@74.97Hz, 0x0, 1"
-        "DP-3, 1920x1080@74.97Hz, -1920x0, 1"
-        "DP-2, 1600x900, -275x-900, 1"
+        #"HDMI-A-1, 1920x1080@74.97Hz, 0x0, 1"
+        #"DP-3, 1920x1080@74.97Hz, -1920x0, 1"
+        #"DP-2, 1600x900, -275x-900, 1"
+        "HDMI-A-1, 1920x1080@100.00Hz, 0x0, 1"
       ]
       # Nexora
       else if osConfig.networking.hostName == "nexora"
