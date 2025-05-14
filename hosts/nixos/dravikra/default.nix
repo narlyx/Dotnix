369e6cc3f326fe9f6d2e9e77a997d486e96a7590
Narlyx <109835029+narlyx@users.noreply.github.com>
2025-05-13 21:20:56 -0600

    Fix: Dravikra mysql connection permissions

--- hosts/nixos/dravikra/default.nix
+++ hosts/nixos/dravikra/default.nix
@@ -11,6 +11,17 @@
   services.mysql = {
     enable = true;
     package = pkgs.mariadb;
+    configFile = pkgs.writeText "my.cnf" ''
+      [mysqld]
+      datadir = /var/lib/mysql
+      bind-address = 0.0.0.0
+      port = 3306
+    '';
+  };
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 3306 ];
+    allowedUDPPorts = [ 3306 ];
   };
 
   networking.hostName = "dravikra";
