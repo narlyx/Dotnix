d32b970e308be48c37dc6bb57244541d4f54b286
Narlyx <109835029+narlyx@users.noreply.github.com>
2025-06-29 15:25:21 -0600

    Feat: Waybar updates

--- home/narlyx/common/wms/hyprland/waybar/default.nix
+++ home/narlyx/common/wms/hyprland/waybar/default.nix
@@ -27,7 +27,38 @@
           format = "{:%a %b %m %I:%M %p}";
           tooltip = false;
         };
+        "battery" = {
+          format = "Bat: {capacity}%";
+        };
+        "wireplumber" = {
+          format = "Vol: {volume}%";
+          max-volume = 100.0;
+          reverse-scrolling = true;
+        };
       }
     ];
+    style = ''
+      * {
+        border-radius: 0;
+        border: none;
+        margin: 0;
+      }
+      window#waybar {
+        border-radius: 0 0 5px 5px;
+      }
+      window#waybar>box {
+        padding: 0 10px 0 10px;
+      }
+      #clock {
+        border-radius: 0 0 5px 0;
+      }
+      #battery {
+        border-radius: 0 0 0 5px;
+      }
+      #tray {
+        margin-right: 5px;
+        border-radius: 0 0 5px 5px;
+      }
+    '';
   };
 }
