215addc015c4d9fcd767001cdb60584c90a9bf5e
Narlyx <shout@narlyx.dev>
2025-07-28 13:11:51 -0600

    Fix: Clipboard support for vim

--- home/narlyx/common/features/vim/default.nix
+++ home/narlyx/common/features/vim/default.nix
@@ -7,6 +7,9 @@
       number = true;
       relativenumber = true;
     };
+    extraConfig = ''
+      set clipboard=unnamedplus
+    '';
     plugins = with pkgs.vimPlugins; [
       vim-tmux-navigator
     ];
