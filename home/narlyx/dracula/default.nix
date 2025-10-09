cbd2db30bbb1b2fccf14e80c0f91e00355e0fad1
Narlyx <shout@narlyx.dev>
2025-10-09 11:49:02 -0600

    Feat: Dracula display configuration

--- home/narlyx/dracula/default.nix
+++ home/narlyx/dracula/default.nix
@@ -6,7 +6,32 @@
     ../common/features/wezterm
   ];
 
+  programs.autorandr = {
+    enable = true;
+    profiles = {
+      "default" = {
+        fingerprint = {
+          eDP-1 = "--CONNECTED-BUT-EDID-UNAVAILABLE--eDP-1";
+        };
+        config = {
+          eDP-1 = {
+            enable = true;
+            primary = true;
+            mode = "2560x1600";
+            rate = "60.00";
+            position = "0x0";
+            scale = {
+              x = 0.82;
+              y = 0.82;
+            };
+          };
+        };
+      };
+    };
+  };
+
   home.packages = with pkgs; [
     brave
+    vesktop
   ];
 }
