9c6acadd9a78df31fe8f0ec32de1c56c9ac5ad3a
Narlyx <shout@narlyx.dev>
2025-11-19 16:57:33 -0700

    Feat: Addition hosting on Krylith

--- hosts/krylith/default.nix
+++ hosts/krylith/default.nix
@@ -27,7 +27,67 @@
   # Services
   services = {
 
-    # Nextcloud
+    # Dashboard
+    glance = {
+      enable = true;
+      settings.server = {
+        host = "0.0.0.0";
+        port = 8080;
+      };
+      settings.pages = [
+        {
+          name = "The Narlyx Network";
+          width = "slim";
+          hide-desktop-navigation = true;
+          center-vertically = true;
+          columns = [
+            {
+              size = "full";
+              widgets = [
+                {
+                  type = "search";
+                  autofocus = true;
+                }
+                {
+                  type = "monitor";
+                  cache = "1m";
+                  title = "Services";
+                  sites = [
+                    {
+                      title = "Homepage";
+                      url = "https://narlyx.net";
+                      icon = "sh:glance";
+                    }
+                    {
+                      title = "Nextcloud";
+                      url = "https://nextcloud.narlyx.net";
+                      icon = "sh:nextcloud";
+                    }
+                    {
+                      title = "Vaultwarden";
+                      url = "https://vault.narlyx.net";
+                      icon = "sh:bitwarden";
+                    }
+                    {
+                      title = "N8N";
+                      url = "https://n8n.narlyx.net";
+                      icon = "sh:n8n";
+                    }
+                    {
+                      title = "Proxmox";
+                      url = "https://proxmox.narlyx.net";
+                      icon = "sh:proxmox";
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
+        }
+      ];
+    };
+
+    # Files
     nextcloud = {
       enable = true;
       hostName = "0.0.0.0";
@@ -42,7 +102,7 @@
       config = {
         dbtype = "sqlite";
         adminuser = "admin";
-        adminpassFile = "/mnt/data/nextcloud/passphrase";
+        adminpassFile = "/mnt/data/secrets/nextcloud-admin-pass";
       };
       settings = {
         trusted_domains = [ "100.64.0.12" "krylith" "krylith.narlyx.tailnet" "nextcloud.narlyx.net" ];
@@ -50,10 +110,34 @@
     };
 
     # Document server
-    onlyoffice = {
+    #onlyoffice = {
+    #  enable = true;
+    #  hostname = "localhost";
+    #  port = 8000;
+    #};
+
+    # Password manager
+    vaultwarden = {
       enable = true;
-      hostname = "localhost";
-      port = 8000;
+      environmentFile = "/mnt/data/secrets/vaultwarden-environment";
+      backupDir = "/mnt/data/appdata/vaultwarden";
+      config = {
+        DOMAIN = "https://vault.narlyx.net";
+        SIGNUPS_ALLOWED = false;
+        ROCKET_ADDRESS = "0.0.0.0";
+        ROCKET_PORT = 8222;
+        ROCKET_LOG = "critical";
+      };
+    };
+
+    # Automation
+    n8n = {
+      enable = true;
+      environment = {
+        N8N_PORT = 5678;
+        #N8N_USER_FOLDER = "/mnt/data/appdata/n8n"; read-only value?!
+        N8N_SECURE_COOKIE = "false";
+      };
     };
 
   };
