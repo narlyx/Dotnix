8b45f3a841410a1139ab3a3df928735880a159f1
Narlyx <shout@narlyx.dev>
2026-01-05 18:44:41 -0700

    Fix: Changed scripts to bash

--- scripts/reload-home.sh
+++ scripts/reload-home.sh
@@ -1,3 +1,3 @@
-#! /usr/bin/env zsh
+#! /usr/bin/env bash
 FLAKE_DIR="$(cd "$(dirname "$0")"/../ && pwd)"
 nix run home-manager -- switch --flake $FLAKE_DIR -b bak
--- scripts/reload-system.sh
+++ scripts/reload-system.sh
@@ -1,3 +1,3 @@
-#! /usr/bin/env zsh
+#! /usr/bin/env bash
 FLAKE_DIR="$(cd "$(dirname "$0")"/../ && pwd)"
 sudo nixos-rebuild switch --flake $FLAKE_DIR
