46ea7ba4007652918c435115608bdfee833cd9c7
Narlyx <shout@narlyx.dev>
2026-01-17 15:33:08 -0700

    Feat: Setup Wireguard on Astylx

--- hosts/astrylx/default.nix
+++ hosts/astrylx/default.nix
@@ -1,4 +1,4 @@
-{
+{ config, ... }: {
     # Imports
     imports = [
         ./hardware-configuration.nix
@@ -17,6 +17,40 @@
             "nofail"
         ];
     };
+
+	# WireGuard
+	networking.wireguard.enable = true;
+	networking.wireguard.interfaces = {
+		wg0 = {
+			ips = [ "10.74.7.41/32" "fc00:bbbb:bbbb:bb01::b:728/128" ];
+			listenPort = 51820;
+			privateKeyFile = "/mnt/data/secrets/mullvad-wg.key";
+			peers = [
+				{
+					publicKey = "SE7HGeByhTo8Ak7FGsjvrYOUJTydQ2L8fWjo17IvhSw=";
+					allowedIPs = [ "0.0.0.0/0" "::/0" ];
+					endpoint = "69.4.234.10:51820";
+					persistentKeepalive = 25;
+				}
+			];
+		};
+	};
+	networking.interfaces.enp1s0.ipv4.routes = [
+		{
+			address = "69.4.234.10";
+			prefixLength = 32;
+			via = "192.168.0.1";
+		}
+	];
+
+	# Firewall
+	networking.firewall = {
+		enable = true;
+		allowedTCPPorts = [ 22 ];
+		allowedUDPPorts = [ 
+			config.networking.wireguard.interfaces.wg0.listenPort
+		];
+	};
 
     # Hostname
     networking.hostName = "astrylx";
