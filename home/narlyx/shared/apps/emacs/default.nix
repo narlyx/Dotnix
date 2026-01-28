2b779e21a1e04ea3a45d2046bd0b6f3d5b5260f9
Narlyx <shout@narlyx.dev>
2026-01-27 20:21:39 -0700

    Feat: Emacs updates

--- home/narlyx/shared/apps/emacs/default.nix
+++ home/narlyx/shared/apps/emacs/default.nix
@@ -1,15 +1,16 @@
 { pkgs, ... }: {
-	services.emacs.enable = true;
-	programs.emacs = {
-		enable = true;
-		package = pkgs.emacs-gtk;
-		extraPackages = epkgs: with epkgs; [
-			use-package
-			evil
-			kanagawa-themes
-			nerd-icons
-			doom-modeline
-		];
-		extraConfig = builtins.readFile ./init.el;
-	};
+    services.emacs.enable = true;
+    programs.emacs = {
+	enable = true;
+	package = pkgs.emacs-gtk;
+	extraPackages = epkgs: with epkgs; [
+	    use-package
+	    evil
+	    good-scroll
+	    nerd-icons
+	    kanagawa-themes
+	    doom-modeline
+	];
+	extraConfig = builtins.readFile ./init.el;
+    };
 }
--- home/narlyx/shared/apps/emacs/init.el
+++ home/narlyx/shared/apps/emacs/init.el
@@ -1,12 +1,22 @@
 ;; Packages are managed by nix
 
 ;; General configurations
-(setq make-backup-files nil) ; stop creating ~ files
+(setq make-backup-files nil) ; stop creating backup files
+(setq auto-save-list-file-prefix "~/.emacs.d/autosave/") ; hide autosave files
+(setq auto-save-file-name-transforms
+      '((".*" "~/.emacs.d/autosave/" t)))
 (setq ring-bell-function 'ignore) ; "shut up bird"
 
+;; Tabs
+(setq tab-stop-list (number-sequence 4 200 4))
+(setq tab-width 4)
+(setq indent-line-function 'insert-tab) 
+(setq indent-tabs-mode t
+	tab-width 4)
+
 ;; Gui configurations
-(load-theme 'kanagawa-wave t) ; theme
 (setq inhibit-splash-screen t) ; remove splash screen
+(setq inhibit-startup-message t) ; remove startup screen
 (menu-bar-mode -1) ; hide menubar
 (tool-bar-mode -1) ; hide toolbar
 (scroll-bar-mode -1) ; hide scrollbar
@@ -15,11 +25,18 @@
 ;; Icons
 (require 'nerd-icons)
 
+;; Theme
+(load-theme 'kanagawa-wave t)
+
 ;; Doom modeline
-(require `doom-modeline)
+(require 'doom-modeline)
 (doom-modeline-mode 1)
 (setq doom-modeline-height 35)
 (setq doom-modeline-buffer-file-name-style 'auto)
+
+;; Good scrolling
+(require `good-scroll)
+(good-scroll-mode 1)
 
 ;; Cua bindings
 (cua-mode t)
@@ -34,3 +51,4 @@
 (setq evil-want-C-i-jump nil) ; restore tab functionality within evil
 (require 'evil)
 (evil-mode 1)
+(define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
